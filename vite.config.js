7da55febc46db0fe44e9b7b59787f5f386307493
bhaktivankhade09 <bhaktivankhade09@gmail.com>
2026-03-20 20:39:29 +0530

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/certificateverifier/',
   define: {
     global: 'globalThis',
-	base: '/certificateverifier/', 
   },
 })
